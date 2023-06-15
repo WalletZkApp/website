777a4ea51d5052e41963b1929ab847001bd1ff05
iam-dev <>
2023-06-15 18:12:41 +0200

    Add GoogleAnalytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,6 +8,8 @@ import { NextIntlClientProvider } from "next-intl";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import "react-toastify/dist/ReactToastify.css";
 
+import GoogleAnalytics from "@/components/google/GoogleAnalytics"; 
+
 export function generateStaticParams() {
   return [
     { locale: "en" },
@@ -33,15 +35,15 @@ export default async function LocaleLayout({
     {},
     await import(`../../messages/${locale}.json`)
   );
-
   return (
     <html lang={locale}>
       <head>
         <title>ZK Keyless Wallet</title>
         <meta
           name="description"
-          content="With a ZK Keyless Wallet, the transaction details are encrypted and the zero-knowledge proof ensures that the transaction is valid"
+          content="The First Smart Contract Wallet on Mina Protocol, No Recovery Seed Phrase Required"
         />
+        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />
       </head>
       <body suppressHydrationWarning={true}>
         <NextIntlClientProvider locale={locale} messages={messages}>
