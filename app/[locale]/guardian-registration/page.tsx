c9a4b9cdb8018f4ceec2e627179420f0f3463832
Abhishek <akanth1994@gmail.com>
2023-07-14 23:14:33 +0530

    form success

--- app/[locale]/guardian-registration/page.tsx
+++ app/[locale]/guardian-registration/page.tsx
@@ -11,14 +11,15 @@ import { useTranslations } from "next-intl";
 
 // Context
 import { ThemeContext } from "@/context/theme_context";
-import { FormEvent, useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WalletContext } from "@/context/wallet_context";
 import Footer from "@/components/layout/footer";
+import { CheckBox } from "@mui/icons-material";
 
 function Page() {
   const { theme } = useContext(ThemeContext);
-  const { getAccounts, account } = useContext(WalletContext);
-
+  const { account } = useContext(WalletContext);
+  const [submitted, setSubmitted] = useState(false);
   const t = useTranslations("Index");
 
   useEffect(() => {}, []);
@@ -46,6 +47,9 @@ function Page() {
           "Content-Type": "application/json",
         },
       });
+      if (response.ok) {
+        setSubmitted(true);
+      }
     }
   }
 
@@ -57,138 +61,160 @@ function Page() {
         } transition-all`}
       >
         <Navigation />
-        <div className="min-h-screen flex items-center justify-center text-center">
-          <div className="flex flex-col max-w-7xl p-5 md:py-12 mx-auto w-full">
-            <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold">
-              {t("Connect Your Wallet And Register")}
+        {!submitted ? (
+          <div className="min-h-screen flex items-center justify-center text-center">
+            <div className="flex flex-col max-w-7xl p-5 md:py-12 mx-auto w-full">
+              <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold">
+                {t("Connect Your Wallet And Register")}
+              </div>
+              <div className="flex justify-center">
+                <div className="bg-primary rounded-full flex justify-center mt-5">
+                  <IconButton>
+                    <WalletIcon sx={{ fontSize: "3rem", color: "white" }} />
+                  </IconButton>
+                </div>
+              </div>
+              <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold mt-6">
+                {t("Guardian Registration")}
+              </div>
+              <form
+                className={`flex flex-col space-y-6 mt-12 ${
+                  theme === "light" ? "text-black" : "text-white"
+                }`}
+                onSubmit={formSubmit}
+              >
+                <input
+                  className="p-5 border bg-transparent"
+                  id="companyName"
+                  placeholder="Company Name"
+                  autoComplete="off"
+                  required
+                  maxLength={40}
+                  minLength={5}
+                />
+                <input
+                  type="number"
+                  className="p-5 border bg-transparent"
+                  id="registrationNumber"
+                  placeholder="Registration Number"
+                  autoComplete="off"
+                  required
+                  maxLength={15}
+                  minLength={5}
+                />
+                <input
+                  className="p-5 border bg-transparent"
+                  id="companyShortDescription"
+                  placeholder="Company Short Description"
+                  autoComplete="off"
+                  required
+                  maxLength={50}
+                  minLength={5}
+                />
+                <input
+                  type="number"
+                  className="p-5 border bg-transparent"
+                  id="phoneNumber"
+                  placeholder="Phone Number"
+                  autoComplete="off"
+                  required
+                  maxLength={15}
+                  minLength={5}
+                />
+                <input
+                  className="p-5 border bg-transparent"
+                  id="address"
+                  placeholder="Address"
+                  autoComplete="off"
+                  required
+                  maxLength={120}
+                  minLength={5}
+                />
+                <input
+                  className="p-5 border bg-transparent"
+                  id="city"
+                  placeholder="City"
+                  autoComplete="off"
+                  required
+                  maxLength={30}
+                  minLength={3}
+                />
+                <input
+                  className="p-5 border bg-transparent"
+                  id="state"
+                  placeholder="State / Province"
+                  autoComplete="off"
+                  required
+                  maxLength={30}
+                  minLength={3}
+                />
+                <input
+                  type="number"
+                  className="p-5 border bg-transparent"
+                  id="zip"
+                  placeholder="ZIP / Postal Code"
+                  autoComplete="off"
+                  required
+                  maxLength={10}
+                  minLength={3}
+                />
+                <input
+                  className="p-5 border bg-transparent"
+                  id="country"
+                  placeholder="Country"
+                  autoComplete="off"
+                  required
+                  maxLength={40}
+                  minLength={3}
+                />
+                <input
+                  type="email"
+                  className="p-5 border bg-transparent"
+                  id="emailAddress"
+                  placeholder="Email Address"
+                  required
+                  maxLength={40}
+                  minLength={5}
+                />
+                <input
+                  type="text"
+                  className="p-5 border bg-transparent"
+                  id="website"
+                  placeholder="Website https://"
+                  required
+                  maxLength={40}
+                  minLength={5}
+                />
+                <button
+                  type="submit"
+                  className="py-4 bg-primary hover:brightness-[1.1] text-white rounded-md"
+                >
+                  Register
+                </button>
+              </form>
             </div>
-            <div className="flex justify-center">
-              <div className="bg-primary rounded-full flex justify-center mt-5">
-                <IconButton>
-                  <WalletIcon sx={{ fontSize: "3rem", color: "white" }} />
-                </IconButton>
+          </div>
+        ) : (
+          <>
+            <div className="min-h-screen flex items-center justify-center text-center">
+              <div className="flex flex-col max-w-7xl p-5 md:py-12 mx-auto w-full">
+                <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold">
+                  {t("Guardian registration completed")}
+                </div>
+                <div className="flex justify-center">
+                  <div className="bg-primary rounded-full flex justify-center mt-5">
+                    <IconButton>
+                      <CheckBox sx={{ fontSize: "3rem", color: "white" }} />
+                    </IconButton>
+                  </div>
+                </div>
+                <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold mt-6">
+                  {t("Please confirm your Email")}
+                </div>
               </div>
             </div>
-            <div className="text-gd text-[1.25rem] md:text-[2rem] font-semibold mt-6">
-              {t("Guardian Registration")}
-            </div>
-            <form
-              className={`flex flex-col space-y-6 mt-12 ${
-                theme === "light" ? "text-black" : "text-white"
-              }`}
-              onSubmit={formSubmit}
-            >
-              <input
-                className="p-5 border bg-transparent"
-                id="companyName"
-                placeholder="Company Name"
-                autoComplete="off"
-                required
-                maxLength={40}
-                minLength={5}
-              />
-              <input
-                type="number"
-                className="p-5 border bg-transparent"
-                id="registrationNumber"
-                placeholder="Registration Number"
-                autoComplete="off"
-                required
-                maxLength={15}
-                minLength={5}
-              />
-              <input
-                className="p-5 border bg-transparent"
-                id="companyShortDescription"
-                placeholder="Company Short Description"
-                autoComplete="off"
-                required
-                maxLength={50}
-                minLength={5}
-              />
-              <input
-                type="number"
-                className="p-5 border bg-transparent"
-                id="phoneNumber"
-                placeholder="Phone Number"
-                autoComplete="off"
-                required
-                maxLength={15}
-                minLength={5}
-              />
-              <input
-                className="p-5 border bg-transparent"
-                id="address"
-                placeholder="Address"
-                autoComplete="off"
-                required
-                maxLength={120}
-                minLength={5}
-              />
-              <input
-                className="p-5 border bg-transparent"
-                id="city"
-                placeholder="City"
-                autoComplete="off"
-                required
-                maxLength={30}
-                minLength={3}
-              />
-              <input
-                className="p-5 border bg-transparent"
-                id="state"
-                placeholder="State / Province"
-                autoComplete="off"
-                required
-                maxLength={30}
-                minLength={3}
-              />
-              <input
-                type="number"
-                className="p-5 border bg-transparent"
-                id="zip"
-                placeholder="ZIP / Postal Code"
-                autoComplete="off"
-                required
-                maxLength={10}
-                minLength={3}
-              />
-              <input
-                className="p-5 border bg-transparent"
-                id="country"
-                placeholder="Country"
-                autoComplete="off"
-                required
-                maxLength={40}
-                minLength={3}
-              />
-              <input
-                type="email"
-                className="p-5 border bg-transparent"
-                id="emailAddress"
-                placeholder="Email Address"
-                required
-                maxLength={40}
-                minLength={5}
-              />
-              <input
-                type="text"
-                className="p-5 border bg-transparent"
-                id="website"
-                placeholder="Website https://"
-                required
-                maxLength={40}
-                minLength={5}
-              />
-              <button
-                type="submit"
-                className="py-4 bg-primary hover:brightness-[1.1] text-white rounded-md"
-              >
-                Register
-              </button>
-            </form>
-          </div>
-        </div>
+          </>
+        )}
         <Footer />
       </div>
     </>
--- app/api/guardian/route.ts
+++ app/api/guardian/route.ts
@@ -5,6 +5,6 @@ const BASE_URL = process.env.ZK_WALLET_USER_SERVICE;
 
 export async function POST(request: NextRequest) {
   const res = await request.json();
-  const response = await axios.post(`${BASE_URL}/guardians`, res);
+  const response = await axios.post(`${BASE_URL}guardians`, res);
   return NextResponse.json(response);
 }
